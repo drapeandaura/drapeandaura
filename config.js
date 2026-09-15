c656b5b1a5d05ceee35974412e500cf4f139971a
drapeandaura <sarojwedsmanish@gmail.com>
2026-09-15 14:53:54 +0530

    Add WhatsApp number to configuration

--- config.js
+++ config.js
@@ -2,3 +2,4 @@
 // Never use the HandmadeWithLove Supabase URL/key here.
 window.SUPABASE_URL="https://rdcseezglpcnfjfgrdxn.supabase.co";
 window.SUPABASE_ANON_KEY="sb_publishable_EBUo4v7PmVaZnWtrcx7vWA_R1kaQBGW";
+window.WHATSAPP_NUMBER="918828078702";
